c4ead6c21c71be498958e4914eb35de0fae765e6
epayet <jakspinning@gmail.com>
2014-05-26 17:15:27 +0200

    Création imprimante

--- ServeurImpression/Document.cs
+++ ServeurImpression/Document.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace ServeurImpression
 {
-    class Serveur
+    public class Document
     {
-
     }
 }
---
+++ ServeurImpression/Imprimante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServeurImpression
+{
+    public class Imprimante
+    {
+        public String Nom { get; set; }
+        public int PagesParMinute { get; set; }
+        public Etat Etat { get; set; }
+        public List<Document> DocumentsEnAttente { get; set; }
+        public Document DocumentEnCours { get; set; }
+        public List<Document> DocumentsEnErreur { get; set; }
+
+        public void Imprimer()
+        {
+
+        }
+
+        public int TempsPrévu(Document doc)
+        {
+            return -1;
+        }
+    }
+}
