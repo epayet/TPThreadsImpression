623536c9da81186559dee4dd319a3b9efcfb1b78
jmgolfier <jmgolfier@gmail.com>
2014-05-27 15:11:26 +0200

    update program.cs

--- ServeurImpression/Program.cs
+++ ServeurImpression/Program.cs
@@ -10,6 +10,15 @@ namespace ServeurImpression
     {
         static void Main(string[] args)
         {
+            Serveur serv = new Serveur();
+            Imprimante Imp = new Imprimante
+            {
+                Nom = "imp1",
+                PagesParMinute = 2
+            };
+            serv.AjouterImprimante(Imp);
+            Document doc = new Document("doc1", new byte[10000]);
+            serv.AjouterLeDocumentALImprimanteQuiPrendLeMoinsDeTemps(doc);
         }
     }
 }
