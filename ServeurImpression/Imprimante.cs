de32779fac987b6b7d10b094b740f620c87abb20
epayet <jakspinning@gmail.com>
2014-05-27 11:31:08 +0200

    Première implémentation imprimante

--- ServeurImpression/Imprimante.cs
+++ ServeurImpression/Imprimante.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServeurImpression
 {
     public class Imprimante
     {
-        public String Nom { get; set; }
-        public int PagesParMinute { get; set; }
+        public string Nom { get; set; }
+        public float PagesParMinute { get; set; }
         public Etat Etat { get; set; }
-        public List<Document> DocumentsEnAttente { get; set; }
-        public Document DocumentEnCours { get; set; }
+        public Queue<Document> DocumentsEnAttente { get; set; }
         public List<Document> DocumentsEnErreur { get; set; }
+        public int NbPagesRestantes { get; set; }
 
         public void Imprimer()
         {
+            Document documentEnCours = DocumentsEnAttente.Dequeue();
 
+            NbPagesRestantes = documentEnCours.GetNbPages();
+            float tempsDImpression = getTempsPrévuPourDoc(documentEnCours);
+            float tempsDImpressionPourUnePage = tempsDImpression / NbPagesRestantes;
+            while (NbPagesRestantes != 0)
+            {
+                Thread.Sleep((int)(tempsDImpressionPourUnePage / 1000));
+                NbPagesRestantes--;
+            }
+
+            documentEnCours = null;
         }
 
-        public int TempsPrévu(Document doc)
+        public float TempsPrévu(Document doc)
         {
-            return -1;
+            return TempsTotalPourDocumentsEnAttente() + getTempsRestantDocEnCours();
+        }
+
+        public float TempsTotalPourDocumentsEnAttente()
+        {
+            float temps = 0;
+            List<Document> documentsEnAttente = new List<Document>(DocumentsEnAttente);
+            foreach(Document docEnAttente in documentsEnAttente) 
+            {
+                temps += getTempsPrévuPourDoc(docEnAttente);
+            }
+            return temps;
+        }
+
+        public void AjouterDocumentAImprimer(Document doc)
+        {
+            DocumentsEnAttente.Enqueue(doc);
+        }
+
+        private float getTempsPrévuPourDoc(Document doc)
+        {
+            return (doc.GetNbPages() * PagesParMinute) / 60;
+        }
+
+        private float getTempsRestantDocEnCours()
+        {
+            return (NbPagesRestantes * PagesParMinute) / 60;
         }
     }
 }
