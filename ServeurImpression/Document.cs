deae98aec6f0e4663c9c137e9738cf2eac75be0b
jmgolfier <jmgolfier@gmail.com>
2014-05-27 09:57:00 +0200

    add serveur and document.

--- ServeurImpression/Document.cs
+++ ServeurImpression/Document.cs
@@ -8,5 +8,16 @@ namespace ServeurImpression
 {
     public class Document
     {
+        public byte[] document;
+        public Document( byte[] source)
+        {
+            document = source;
+        }
+
+        public int nbrPages()
+        {
+            return document.Length / 100000;
+        }
+    
     }
 }
--- ServeurImpression/Serveur.cs
+++ ServeurImpression/Serveur.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ServeurImpression
 {
     class Serveur
     {
+        public const string PATH = "c:\tpImpression";
 
+        string ajouter(byte[] source)
+        {
+            Document doc = new Document(source);
+            return "";
+        }
     }
 }
